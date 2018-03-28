1155c4e5e6350a2b166ea4b1492393e10c31864f
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-28 04:03:48 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
-
-  create_table "actors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "character_id"
-    t.integer "show_id"
-  end
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
