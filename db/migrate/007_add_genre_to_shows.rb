bac07c042ac31a734e46174c25c0127078e41c06
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-28 04:01:27 +0000

    automatically backed up by learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,8 +1,5 @@
 class AddGenreToShows < ActiveRecord::Migration[5.1]
   def change
-    create_table :characters do |t|
-      t.string :name
-      t.string :genre
-    end
+    add_column :shows, :genre, :string
   end
 end
