6807fec8128f1d04464139750449b62d95e17bba
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-28 03:47:40 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,13 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows
+
+  def self.full_name
+    self.first_name
+    self.last_name
+  end
+
+  def list_roles
+    self.characters
+  end
 end
